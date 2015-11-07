4b12339da5eb4ea8fbad34ea06b8d498aed6d390
humanitiesNerd <catonano@gmail.com>
2015-11-07 20:23:34 +0100

    a fingernail far away from a working state

--- build.boot
+++ build.boot
@@ -2,7 +2,8 @@
   :project 'semantic-arpa
   :version "0.1.0-SNAPSHOT"
   :dependencies '[[org.clojure/tools.namespace "0.2.11"]
-                  [org.clojure/data.csv "0.1.3"]]
+                  [org.clojure/data.csv "0.1.3"]
+                  [clj-time "0.11.0"]]
   :exclusions '[]
   :source-paths #{"src"}
   :resource-paths #{"resources"}
--- src/semantic_arpa/core.clj
+++ src/semantic_arpa/core.clj
@@ -1,14 +1,107 @@
 (ns semantic_arpa.core
   (:require
    [clojure.java.io :as io]
-   [clojure.data.csv :as csv]))
+   [clojure.data.csv :as csv]
+   [clj-time.format :as f]
+   [clj-time.core :as t]))
 
+(defn writelines [file-path lines]
+  (with-open [out-file (io/writer file-path)]
+    (doseq [line lines] (.write out-file line))))
+
+(def reconciliated-values
+  {"NO2" {
+          :dbpedia "Nitrogen_dioxide"
+          :dbpedia-it "Diossido_di_azoto"}
+   "CO" {
+         :dbpedia "Carbon_monoxide"
+         :dbpedia-it "Monossido_di_carbonio"}
+   "HC" {
+         :dbpedia "Hydrocarbon"
+         :dbpedia-it "Idrocarburi"}
+   "NO" {
+         :dbpedia "Nitric_oxide"
+         :dbpedia-it "Monossido_di_azoto" }
+   "NOx" {
+          :dbpedia "NOx"
+          :dbpedia-it "NOx"}
+   "O3" {
+         :dbpedia "Ozone"
+         :dbpedia-it "Ozono" }
+   "PM10" {
+           :dbpedia "Particulates"
+           :dbpedia-it "PM10" }
+   "SO2" {
+          :dbpedia "Sulfur_dioxide"
+          :dbpedia-it "Diossido_di_zolfo"}
+   "CH4" {
+          :openarpa "CH4"}
+   "H2S" {
+          :openarpa "H2S"}
+   "NMHC" {
+           :openarpa "NMHC"}
+   })
+
+;"\t" "ssn:observerdProperty " "\"" substance "\"" " ;" "\n"
+(defn observedProperties [substance]
+  (let [stuff (reconciliated-values substance)]
+    (mapv (fn [item] (str (name (first item)) ":" (second item) " ,"))  stuff)
+    ))
+
+(def multiparser (f/formatter-local "dd/MM/YYYY HH:mm"))
+(def id-formatter (f/formatter-local "ddMMYYYYHHmmss"))
+(def rdf-formatter (f/formatter-local "dd/MM/YYYY HH:mm:ss"))
+(def daytime-formatter (f/formatter-local "HH:mm:ss"))
+
+(defn parsed-datetime [datetime]
+  (f/parse-local multiparser datetime))
+
+(defn unparsed-datetime [formatter datetime]
+  (f/unparse-local formatter datetime))
+
+(defn year [datetime]
+  (t/year datetime))
+
+(defn month [datetime]
+  (t/month datetime))
+
+(defn day [datetime]
+  (t/day datetime))
 
 (defn lines [path]
   (csv/read-csv (io/reader (io/file path))))
 
 (defn as-ttl [[datetime substance value measure station lat lon :as record]]
-  record)
+  (let [parsed-datetime (parsed-datetime datetime)]
+    [
+     (str "\n" "openarpa-obs:" station substance
+          (unparsed-datetime id-formatter parsed-datetime)
+          " a " "ssn:Observation" " ;" "\n")
+     
+     
+     (str "\t" "ssn:observationResultTime "
+          "\"" (unparsed-datetime rdf-formatter parsed-datetime) "\""
+          "^^xsd:dateTime ;" "\n")
+
+     (str "\t" "ssn:isProducedBy openarpa-sens:" station " ;" "\n")
+     (str "\t" "time:month " "\"" (month parsed-datetime) "\"" "^^xsd:gMonth ;" "\n")
+     (str "\t" "time:year " "\"" (year parsed-datetime) "\"" "^^xsd:gYear ;" "\n")
+     (str "\t" "time:day " "\"" (month parsed-datetime) "\"" "^^xsd:gDay ;" "\n")
+     (str "\t" "time:inDateTime " "\"" (unparsed-datetime daytime-formatter parsed-datetime) "\"" "^^xsd:time ;" "\n")
+     
+            (into
+             (vector "\t" "ssn:observerdProperty " "\"" substance "\"" " ,")
+             ;(observedProperties substance)
+             (vector " ;" "\n"))
+
+     (str "\t" "basic:uom " "\"" measure "\"" " ;" "\n")
+     
+     ]
+    )
+  )
 
 (defn main [path]
-  (map as-ttl (lines path)))
+  (writelines "resources/openarpa.ttl"
+              (mapcat as-ttl (lines path))
+              ))
+  
